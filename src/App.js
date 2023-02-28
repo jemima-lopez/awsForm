c14cf0fd92515e502e29df882e62df2f93c0924c
jemima-lopez <jlopezj11@miumg.edu.gt>
2023-02-28 11:42:15 -0600

    commit number one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import {API, graphqlOperation } from 'aws-amplify';
+import {createTask} from './graphql/mutations'
+import { listTasks } from "./graphql/queries";
+import { withAuthenticator, Button, Heading, Alert } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+function App({signOut, user}) {
+  const [ task, setTask] = useState({
+    name: "",
+    description: "",
+  });
+  const [tasks, setTasks] = useState([]);
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    console.log(task);
+    const result = await API.graphql(graphqlOperation(createTask, {input: task}));
+    console.log(result);
+  };
+
+  useEffect(() => {
+    async function looadTasks(){
+      const result = await API.graphql(graphqlOperation(listTasks));
+      setTasks(result.data.listTasks.items);
+    }
+    looadTasks();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Alert variation="info">Welcome</Alert>
+  <Heading level={1}>Hello {user.username}</Heading>
+    <form onSubmit={handleSubmit}>
+      <input 
+      name='name' 
+      placeholder='Title'
+      onChange={(e) => setTask({ ...task, name: e.target.value})}
+      ></input>
+      <textarea 
+      name='description' 
+      placeholder='Description'
+      onChange={(e) => setTask({ ...task, description: e.target.value})}
+      ></textarea>
+      <button>Submit</button>
+    </form>
+
+    {tasks.map(task => {
+      return <article className = "card card-body">
+        <h3>{task.name}</h3>
+        <p>{task.description}</p>
+        </article>
+    })}
+
+    <Button onClick={signOut}>Sign out</Button>
+    </>
+    
   );
 }
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTask = /* GraphQL */ `
+  subscription OnCreateTask($filter: ModelSubscriptionTaskFilterInput) {
+    onCreateTask(filter: $filter) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTask = /* GraphQL */ `
+  subscription OnUpdateTask($filter: ModelSubscriptionTaskFilterInput) {
+    onUpdateTask(filter: $filter) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTask = /* GraphQL */ `
+  subscription OnDeleteTask($filter: ModelSubscriptionTaskFilterInput) {
+    onDeleteTask(filter: $filter) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
